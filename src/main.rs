426b997281885e00faaad2c2563945fb479f21e1
alebot-dev <qbotdedv84@gmail.com>
2020-12-30 17:59:38 +0000

    getting closer, waiting for veld to implement ws backend

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,70 @@
 use iced::{
-    button, executor, Align::Center, Application, Button, Column, Command, Element, HorizontalAlignment,
-    Length, Row, Settings, Text,
+    button, executor, scrollable, text_input, window, Align::Center, Application, Button, Column,
+    Command, Container, Element, HorizontalAlignment, Length, Scrollable, Settings, Text,
+    TextInput,
 };
 use reqwest::blocking::get;
 
-fn main() -> iced::Result {
-    Hello::run(Settings::default())
+pub fn main() -> iced::Result {
+    let mut settings: Settings<()> = Settings::default();
+    println!("{:?}", settings);
+    settings.window = window::Settings {
+        size: (800, 600),
+        ..Default::default()
+    };
+    MainView::run(settings)
 }
 
-#[derive(Default)]
-struct Hello {
-    button_state: button::State,
+#[derive(Clone, Debug, Default)]
+struct MainView {
+    btn_states: BtnStates,
+    text_states: TextAreaStates,
+    text_values: TextAreaValues,
+    scroll_states: ScrollableStates,
+    messages: Vec<String>,
 }
 
-#[derive(Debug, Clone, Copy)]
-pub enum ButtonPresses {
-    Press1,
+#[derive(Clone, Copy, Debug, Default)]
+struct BtnStates {
+    append_btn_state: button::State,
+    max_btn_state: button::State,
+    min_btn_state: button::State,
+    close_btn_state: button::State,
 }
 
-impl Application for Hello {
+#[derive(Clone, Debug, Default)]
+struct TextAreaStates {
+    message_send_state: text_input::State,
+}
+
+#[derive(Clone, Debug, Default)]
+struct TextAreaValues {
+    message_send_value: String,
+}
+
+#[derive(Clone, Debug, Default)]
+struct ScrollableStates {
+    main_scrollable_state: scrollable::State,
+}
+
+#[derive(Debug, Clone)]
+pub enum Msgs {
+    CloseWindow,
+    LoadingMessage,
+    MaximiseWindow,
+    MinimiseWindow,
+    LoadedMessage(String),
+    SendMessage(String),
+}
+
+impl Application for MainView {
     type Executor = executor::Default;
-    type Message = ButtonPresses;
+    type Message = Msgs;
     type Flags = ();
 
-    fn new(_flags: ()) -> (Hello, Command<Self::Message>) {
+    fn new(_flags: ()) -> (MainView, Command<Self::Message>) {
         (
-            Hello {
+            MainView {
                 ..Default::default()
             },
             Command::none(),
@@ -33,20 +72,37 @@ impl Application for Hello {
     }
 
     fn title(&self) -> String {
-        String::from("A cool application")
+        String::from("with myself | Oh no!!!!!!!!!!!")
     }
 
     fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
         match message {
-            ButtonPresses::Press1 => Command::perform(get_the_page(), move |res| {
-                println!("{:?}", res.unwrap());
-                message
-            }),
+            Msgs::CloseWindow => {
+                std::process::exit(0);
+            }
+            Msgs::MaximiseWindow => Command::none(),
+            Msgs::MinimiseWindow => Command::none(),
+            Msgs::LoadedMessage(h) => {
+                self.messages.push(h);
+                println!("{:?}", self.messages);
+                Command::none()
+            }
+            Msgs::LoadingMessage => {
+                self.update(Msgs::LoadedMessage(format!(
+                    "{:?}",
+                    get_the_page().unwrap()
+                )));
+                Command::none()
+            }
+            Msgs::SendMessage(val) => {
+                self.text_values.message_send_value = val;
+                Command::none()
+            }
         }
     }
 
     fn view(&mut self) -> Element<Self::Message> {
-        Row::new()
+        let main_body: Element<_> = Column::new()
             .padding(20)
             .align_items(Center)
             .width(Length::Fill)
@@ -54,29 +110,46 @@ impl Application for Hello {
                 Text::new("Here is some text lmao")
                     .width(Length::Fill)
                     .horizontal_alignment(HorizontalAlignment::Center)
-                    .size(100)
+                    .size(100),
             )
             .push(
-                Column::new()
-                    .push(
-                        Text::new("Click the button to print stuff to stdout")
-                            .size(35)
-                    )
-                    .push(
-                        Button::new(
-                            &mut self.button_state,
-                            Text::new("This is a button biiiiitch"),
-                        )
-                        .on_press(self::ButtonPresses::Press1),
-                    )
-                    .align_items(Center)
-                    .padding(20)
+                Button::new(
+                    &mut self.btn_states.append_btn_state,
+                    Text::new("Append le h"),
+                )
+                .on_press(Msgs::LoadingMessage),
             )
+            .into();
+        let messages: Element<_> = self
+            .messages
+            .iter_mut()
+            .enumerate()
+            .fold(Column::new(), |column, (_, task)| {
+                column.push(Text::new(format!("{:?}", task)))
+            })
+            .into();
+        let inputs: Element<_> = TextInput::new(
+            &mut self.text_states.message_send_state,
+            "Send a message",
+            &self.text_values.message_send_value,
+            Msgs::SendMessage,
+        )
+        .into();
+        let content = Column::new()
+            .align_items(Center)
+            .spacing(20)
+            .push(main_body)
+            .push(messages)
+            .push(inputs);
+        let container: Element<_> = Container::new(content).into();
+        Scrollable::new(&mut self.scroll_states.main_scrollable_state)
+            .push(container)
             .into()
     }
 }
 
-pub async fn get_the_page() -> std::result::Result<std::collections::HashMap<String, String>, Box<dyn std::error::Error>> {
+pub fn get_the_page(
+) -> std::result::Result<std::collections::HashMap<String, String>, Box<dyn std::error::Error>> {
     let res = get("https://httpbin.org/ip")?.json::<std::collections::HashMap<String, String>>()?;
     Ok(res)
 }
